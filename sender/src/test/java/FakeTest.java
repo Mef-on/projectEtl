1bc95b35e571eb3a8c30f4ac778820f3cdd3fd93
mef-on <m.j.kleczko@gmail.com>
2023-05-22 20:35:20 +0200

    upgrade FaketEST

--- sender/src/test/java/FakeTest.java
+++ sender/src/test/java/FakeTest.java
@@ -1,15 +1,17 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class FakeTest {
     static private int TEST_1 = 1;
     static private int TEST_2 = 2;
 
     @Test
     void fakeTest() {
+        Assertions.assertEquals(TEST_1, TEST_1);
+    }
+
+    @Test
+    void fakeTest1() {
         Assertions.assertEquals(TEST_1, TEST_2);
     }
 }
